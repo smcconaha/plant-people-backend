ebb93bfe6e29d0069e5474199bb88d35bea612c0
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-28 20:54:04 +0000

    Added service and listings models

--- config/plants/models.py
+++ config/plants/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
@@ -6,3 +7,18 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Service(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Listings(models.Model):
+    body = models.CharField(max_length=1000, null=False, blank=False, unique=False)
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
