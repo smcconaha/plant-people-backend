d602fb0e9d9027e94fdd744095df9948436d8d75
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-28 21:04:58 +0000

    Added ListingService pivot table linking service and listing tables

--- config/plants/models.py
+++ config/plants/models.py
@@ -14,11 +14,20 @@ class Service(models.Model):
     def __str__(self):
         return self.name
 
-class Listings(models.Model):
+class Listing(models.Model):
     body = models.CharField(max_length=1000, null=False, blank=False, unique=False)
     created_date = models.DateTimeField(auto_now_add=True, null=True)
     updated_date = models.DateTimeField(auto_now=True, null=True)
+    service = models.ManyToManyField(Service, through='ListingService', related_name="listing_list")
     
     def __str__(self):
         return self.name
 
+class ListingService(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = (
+            ['listing', 'service']
+        )
