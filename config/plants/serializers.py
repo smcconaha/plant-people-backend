d0e9a6b93effe27a213b25ff2aa1f9b3c8483f3e
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-29 13:31:10 +0000

    Added serializer for Service model

--- config/plants/serializers.py
+++ config/plants/serializers.py
@@ -1,3 +1,8 @@
 from rest_framework import serializers
 from .models import *
 from pprint import pprint
+
+class ServiceSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ('name', 'description',)
--- config/plants/views.py
+++ config/plants/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class ServiceSerializer(ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
