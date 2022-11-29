936470ac92d502c2f64918e8d7af9788e1122bb2
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-11-29 13:08:54 +0000

    Updated root URL

--- config/plants/models.py
+++ config/plants/models.py
@@ -10,6 +10,7 @@ class CustomUser(AbstractUser):
 
 class Service(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False, unique=True)
+    description = models.CharField(max_length=255, null=False, blank=False, unique=True)
 
     def __str__(self):
         return self.name
